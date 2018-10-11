de1c683679fb1aba56ef35b48a4b116a1e50f92d
ifdevil <384867771@qq.com>
2018-10-11 22:48:33 +0800

    影片查询

--- guns-api/src/main/java/com/stylefeng/guns/api/film/FilmServiceAPI.java
+++ guns-api/src/main/java/com/stylefeng/guns/api/film/FilmServiceAPI.java
@@ -33,4 +33,16 @@ public interface FilmServiceAPI {
     List<SourceVO> getSources();
     //年代条件
     List<YearVO> getYears();
+
+    //根据影片Id和影片名称获取影片信息
+    FilmDetailVO getFilmDetail(int searchType,String searchParam);
+    //获取影片相关的其他信息【演员表、图片地址...】
+    //获取影片描述信息
+    FilmDescVO getFilmDesc(String filmId);
+    //获取图片信息
+    ImgVO getImgs(String filmId);
+    //获取导演信息
+    ActorVO getDescInfo(String filmId);
+    //获取演员信息
+    List<ActorVO> getActors(String filmId);
 }
---
+++ guns-api/src/main/java/com/stylefeng/guns/api/film/vo/InfoRequstVO.java
@@ -0,0 +1,12 @@
+package com.stylefeng.guns.api.film.vo;
+
+import lombok.Data;
+
+@Data
+public class InfoRequstVO {
+
+    private String biography;
+    private ActorRequestVO actors;
+    private ImgVO imgVO;
+    private String filmId;
+}
