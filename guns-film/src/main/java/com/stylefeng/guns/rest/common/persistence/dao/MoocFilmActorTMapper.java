b703f6bb9577195d0decc055f11f2051c5913a4f
ifdevil <384867771@qq.com>
2018-10-12 23:59:01 +0800

    movie

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocFilmActorTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocFilmActorT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 影片与演员映射表 Mapper 接口
+ * </p>
+ *
+ * @author npz
+ * @since 2018-10-12
+ */
+public interface MoocFilmActorTMapper extends BaseMapper<MoocFilmActorT> {
+
+}
