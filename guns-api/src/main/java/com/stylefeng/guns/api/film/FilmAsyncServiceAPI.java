7f1276b6e12ebf47107676490cbfe7237d0a478b
ifdevil <384867771@qq.com>
2018-10-15 23:11:32 +0800

    影片详情异步调用接口

---
+++ guns-api/src/main/java/com/stylefeng/guns/api/film/FilmAsyncServiceAPI.java
@@ -0,0 +1,17 @@
+package com.stylefeng.guns.api.film;
+
+import com.stylefeng.guns.api.film.vo.*;
+
+import java.util.List;
+
+public interface FilmAsyncServiceAPI {
+    //获取影片相关的其他信息【演员表、图片地址...】
+    //获取影片描述信息
+    FilmDescVO getFilmDesc(String filmId);
+    //获取图片信息
+    ImgVO getImgs(String filmId);
+    //获取导演信息
+    ActorVO getDescInfo(String filmId);
+    //获取演员信息
+    List<ActorVO> getActors(String filmId);
+}
