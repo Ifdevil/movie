8647a6df13066bea15891b95f505fe2e7781bea0
ifdevil <384867771@qq.com>
2018-12-12 00:24:55 +0800

    init mq

--- guns-order/src/main/java/com/stylefeng/guns/rest/mq/OrderProducer.java
+++ guns-order/src/main/java/com/stylefeng/guns/rest/mq/OrderProducer.java
@@ -6,30 +6,41 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
 
 @Slf4j
 @Data
 @Configuration
-@ConfigurationProperties(prefix = "apache")
+@ConfigurationProperties(prefix = "rocketmq")
 public class OrderProducer {
 
-    private DefaultMQProducer orderProduce;
+
     private String nameserveraddress;
     private String producergroup;
 
-    @PostConstruct
-    public void getOrderProduce(){
-        orderProduce = new DefaultMQProducer();
+    @Bean
+    public DefaultMQProducer getOrderProduce(){
+        DefaultMQProducer orderProduce = new DefaultMQProducer();
         orderProduce.setNamesrvAddr(nameserveraddress);
         orderProduce.setProducerGroup(producergroup);
         try {
             orderProduce.start();
+            log.info("初始化消息队列OrderProducer成功");
         } catch (MQClientException e) {
             log.error("初始化消息队列OrderProducer失败",e);
         }
+        return orderProduce;
+    }
+
+    /**
+     * 发送消息
+     * @return
+     */
+    public static boolean sendMessage(){
+
+        return true;
     }
 
 }
