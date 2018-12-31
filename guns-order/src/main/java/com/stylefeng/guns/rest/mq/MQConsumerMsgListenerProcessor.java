89d03d648a0f67cf8bff1de355a42fde01f9492d
ifdevil <384867771@qq.com>
2018-12-31 12:04:59 +0800

    set tryTimes

--- guns-order/src/main/java/com/stylefeng/guns/rest/mq/MQConsumerMsgListenerProcessor.java
+++ guns-order/src/main/java/com/stylefeng/guns/rest/mq/MQConsumerMsgListenerProcessor.java
@@ -20,8 +20,25 @@ public class MQConsumerMsgListenerProcessor implements MessageListenerConcurrent
             log.info("接受到的消息为空，不处理，直接返回成功");
             return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         }
-        for (Message msg : msgs) {
-            System.out.println(new String(msg.getBody()) + ":" + msg.toString());
+        //处理消息
+        for (MessageExt msg : msgs) {
+            try {
+                System.out.println(new String(msg.getBody()) + ":" + msg.toString());
+                String topic = msg.getTopic();
+                if(topic == "order"){
+
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                //假如接受消息时抛出异常，设定重试次数
+                int reconsumTimes = msg.getReconsumeTimes();
+                if(reconsumTimes == 3){
+                    log.info(msg.getMsgId()+"已重试3次，取消重试，补偿处理");
+                    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                }
+                return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+            }
+
         }
         return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
     }
